068b80260498fc100e3407309870fca20d942a24
Juan Pablo Pino Bravo <jp.pinob@gmail.com>
2022-10-25 15:33:21 +0200

    Restore load2

--- src/test/resources/tests/basic/load2.s
+++ src/test/resources/tests/basic/load2.s
@@ -1,8 +1,13 @@
 main:
+	addi x1, zero, 4
+	addi x2, zero, 4
+	addi x3, zero, 4 
+	addi x4, zero, 4 
 	lw x1, 0(x1)
-	nop
-	nop
+	add x1, x1, x1
+	lw x1, 0(x1)
 	sw x1, 4(x1)
+	lw x1, 4(x1)
 	done
 #memset 0x0,  4
 #memset 0x4,  8
